1cc257212afc2c358b0ac8619a51d80c8c591318
Michael.chow <michael.chow@live.com>
2013-08-08 12:23:28 +0800

    update

---
+++ config/memcache.js
@@ -0,0 +1,3 @@
+var  config = Array();
+config['development'] = {host : "localhost", port:"11211"}
+exports = module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-
+var mysqlClient = require('./../library/mysqlclient').init();
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+	var args = null;
+	var sql = "SELECT * FROM bet_award_type";
+	mysqlClient.query(sql,args,function(err, data){
+		res.render('index', { title: data[0].title, data:data });
+	});
+
 };

ce83dba0cca0d9622c2cfa5ebb6e86c2871d044d
home <michael.chow@live.com>
2013-08-17 14:36:57 +0800

    test

--- test.js
+++ test.js
@@ -1,49 +1,22 @@
-var mcClient = require('./library/memcacheclient');
+var fs = require('fs');
+var data = fs.readFileSync('./data.txt').toString();
 
 
-mcClient.set('test', 'wawwwwwwwwwwwww', function(err, data){
-	mcClient.get('test', function(err, data){
-		console.log(data);
-		mcClient.get('test', function(err, data){
-			console.log(data);
-			mcClient.get('test', function(err, data){
-				console.log(data);
-			});
-		});
-	});
-	console.log(data);
-
-});
-
-
-mcClient.get('test222222', function(err, data){
-	console.log(data);
-});
-
-mcClient.close();
-/*
-var memcache = require('memcache');
-
-
-mcClient = new memcache.Client();
-mcClient.connect();
-//mcClient.addHandler(onConnect);
-
-
-
-
-
-var util      = require('util');
-var setKey = function() {
-	mcClient.set('test', 'hello \r\n node-memcache', function(err, response) {
-		mcClient.get('test', function(err, data) {
-			util.debug(data);
-			mcClient.close();
-		});
-	});
-};
-
-setKey();
-*/
-//mcClient.connect();
-//setKey();
+var timePattern = "<td>\\d{2}</td>\r\n.*<td>\\d{4}-\\d{2}-\\d{2} (\\d{2}):(\\d{2})</td>";
+		var timeArray = Array();
+		if (timePattern != '')
+		{
+		console.log(timePattern);
+			var timeReg = new RegExp(timePattern,"ig");
+			
+			while(time = timeReg.exec(data)) {
+				var timeGroup = Array();
+				for(i=1;i<time.length;i++){
+					timeGroup.push(time[i]);
+				}
+				timeArray.push(timeGroup.join(":"));
+			}
+			
+		}
+//console.log(data);
+		console.log(timeArray);

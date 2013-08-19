cce4d41b54e06c28267d70f0da44bd250dc95184
Michael.chow <michael.chow@live.com>
2013-08-19 16:24:17 +0800

    修改提交

--- test.js
+++ test.js
@@ -1,22 +1,31 @@
-var fs = require('fs');
-var data = fs.readFileSync('./data.txt').toString();
+var fs = require("fs"),tz = require("timezone");
+eval(fs.readFileSync('./library/dateformat.js').toString());
+var url = "http://www.wclc.com/app/winning_numbers/keno.html?monthYear=" + tz(require("timezone/" + "Canada/Pacific"))(new Date().getTime(), "%d-%b-%Y ", "Canada/Pacific");
 
+console.log(url);
+canada = tz(require("timezone/" + "Canada/Pacific"));
+var canadaNowHour = parseInt(canada(new Date().getTime(), "%-I", "Canada/Pacific"));
+var firstTime;
+if (canadaNowHour >=4){
+	firstTime = canada(new Date().getTime(), "%F 04:30:00", "Canada/Pacific");
+}else{
+	firstTime = canada(new Date().getTime() - 60*60*24*1000, "%F 04:30:00", "Canada/Pacific");
+}
+var pastDraw = Math.floor((new Date().getTime() - canada(firstTime)) / 1000 / 300);
+console.log(pastDraw);
 
-var timePattern = "<td>\\d{2}</td>\r\n.*<td>\\d{4}-\\d{2}-\\d{2} (\\d{2}):(\\d{2})</td>";
-		var timeArray = Array();
-		if (timePattern != '')
-		{
-		console.log(timePattern);
-			var timeReg = new RegExp(timePattern,"ig");
-			
-			while(time = timeReg.exec(data)) {
-				var timeGroup = Array();
-				for(i=1;i<time.length;i++){
-					timeGroup.push(time[i]);
-				}
-				timeArray.push(timeGroup.join(":"));
-			}
-			
-		}
-//console.log(data);
-		console.log(timeArray);
+console.log(canada(new Date().getTime(), "%F %T", "Canada/Pacific"));
+console.log(canadaNowHour);
+console.log(firstTime);
+
+/*
+
+                    ulong maxRound  = Convert.ToUInt64(GetMaxRound(rounds));//最后的期数
+                    ulong minRound  = Convert.ToUInt64(GetMinRound(rounds));//最后的期数
+                    ulong thisRound = Convert.ToUInt64(round);//当期期数
+	
+
+	
+var url = "http://www.wclc.com/app/winning_numbers/keno.html?monthYear=" + TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time")).ToString("dd-MMM-yyyy", System.Globalization.CultureInfo.InvariantCulture)";
+var delaySecond = 30;//延迟n秒确定开奖 
+*/
